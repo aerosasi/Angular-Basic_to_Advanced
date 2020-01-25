5ba4d8db40671dc309d7a81a4e6e2307268718c8
Sasidharan SD <47988127+aerosasi@users.noreply.github.com>
2020-01-25 15:09:45 +0530

    Added Recipie
    check out recipie-list coponents and html

--- myapp/src/app/recipes/recipe-list/recipe-list.component.ts
+++ myapp/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes:Recipe[]=[
+    new Recipe('Test recipe','a test recipe','https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971556_1280.jpg')
+  ];
 
   constructor() { }
 
